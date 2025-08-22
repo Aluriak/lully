2c7c25095c0cc35606465e4fbfbcffce121e88e0
Lucas Bourneuf <lucas.bourneuf@chu-brest.fr>
2025-08-22 09:58:58 +0200

    feat(passwd): more defensive function + logging

--- lully/passwd.py
+++ lully/passwd.py
@@ -1,10 +1,14 @@
 
 import subprocess
-from typing import Optional
+from typing import Optional, Union
 
-def ask(passpath: str, pass_args: str = '', *, pass_bin_path: str = '/bin/pass', cmd: str = 'show', log: bool = False) -> Optional[str]:
-    print('asking for ', passpath)
-    _cmd = [pass_bin_path, cmd, *pass_args.split(' '), passpath]
+def ask(passpath: str, pass_args: Union[list[str], str] = '', *, pass_bin_path: str = '/bin/pass', cmd: str = 'show', log: bool = False) -> Optional[str]:
+    if log:
+        print('asking for ', passpath)
+    pass_args = [] if not pass_args else pass_args  # default and empty vals are empty list
+    pass_args = pass_args.split(' ') if isinstance(pass_args, str) else pass_args  # str values are casted to list
+    _cmd = [pass_bin_path, cmd, *pass_args, passpath]
+    assert not any(s == '' for s in pass_args), pass_args  # happened once, was annoying
     if log:
         print(f"Will access password with `{' '.join(_cmd)}`…")
     proc = subprocess.Popen(_cmd, stdout=subprocess.PIPE)
@@ -14,6 +18,10 @@ def ask(passpath: str, pass_args: str = '', *, pass_bin_path: str = '/bin/pass',
     if line and line.startswith('Error: ') and line.endswith(' is not in the password store.'):
         if log:
             print(line)
+        return None
+    elif line == 'Password Store':
+        if log:
+            print('Invalid command: got the whole passwords hierarchy')
         return None
     return line
 
