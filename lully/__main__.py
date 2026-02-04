d30217a12776a2301fb5c7505cd86b9f99edc5be
Lucas Bourneuf <lucas.bourneuf@chu-brest.fr>
2026-02-04 10:09:19 +0100

    feat(): remove most of selection bias in the word generator ; demonstration and results with cli command `id`

--- lully/__main__.py
+++ lully/__main__.py
@@ -2,6 +2,7 @@
 
 import uuid
 import argparse
+import itertools
 from collections import Counter, defaultdict
 
 from . import hashing, words, vgauge
@@ -33,12 +34,29 @@ def show_elements_repartition(stats: dict[str, int], elements: list[str] = sorte
                     counts_per_elem[elem_to_key(elem)] += count
         max_counts = max(counts_per_elem.values())
         #print(f"{counts_per_elem=}, {max_counts=} total_count={sum(counts_per_elem.values())}")
-        counts_per_elem = {l: vgauge(counts_per_elem[l] / max_counts) for l in sorted(counts_per_elem)}
+        try:
+            counts_per_elem = {l: vgauge(counts_per_elem[l] / max_counts) for l in sorted(counts_per_elem)}
+        except ZeroDivisionError:
+            counts_per_elem = {l: '.' for l in sorted(counts_per_elem)}
+
         if header:
             ret = ' '*len(leading) + ''.join(str(v) for v in counts_per_elem.keys()) + '\n'
         else:
             ret = ''
         print( ret + leading + ''.join(str(v) for v in counts_per_elem.values()) )
+
+def produce_codes(nb_loop: int, use_uuid:bool = True, **kwargs):
+    codes = []
+    progress_bar = None
+    for idx in range(nb_loop):
+        code = hashing.human_code(uuid.uuid4() if use_uuid else idx, **kwargs)
+        # print(code)
+        codes.append(code)
+        if progress_bar != round(idx / nb_loop * 100):
+            progress_bar = round(idx / nb_loop * 100)
+            print('\r[' + ('█'*progress_bar).ljust(100), ']', end='', sep='', flush=True)
+    print()
+    return codes
 
 
 if __name__ == '__main__':
@@ -47,25 +65,29 @@ if __name__ == '__main__':
     if args.action == 'id':
         nb_comb = len(words.NOUNS) * len(words.ADJECTIVES)
         print(f"{len(words.NOUNS)} nouns × {len(words.ADJECTIVES)} adjectives = {nb_comb} combinations\n")
-        codes = []
         nb_loop = int(args.n or nb_comb)
-        progress_bar = None
-        for idx in range(nb_loop):
-            code = hashing.human_code(uuid.uuid4())
-            # print(code)
-            codes.append(code)
-            if progress_bar != round(idx / nb_loop * 100):
-                progress_bar = round(idx / nb_loop * 100)
-                print('\r[' + ('█'*progress_bar).ljust(100), ']', end='', sep='', flush=True)
-        print()
-        stats = Counter(codes)
-        print(stats.most_common(10))
-        total_count = sum(stats.values())
-        for duplicata, count in sorted(Counter(stats.values()).items(), key=lambda d: d[0]):
-            print(f"{duplicata} × {count}".ljust(14), f"{round(count/total_count*100,3)}% du total")
+        stats = {}
+        for exp, (arg_uuid, arg_sum) in enumerate(itertools.product((True, False), repeat=2), start=1):
+            print(f"\nExperiment {exp} use uuid={arg_uuid} sum={arg_sum}")
+            codes = produce_codes(nb_loop, use_uuid=arg_uuid, method_sum=arg_sum)
+            s = stats[exp] = Counter(codes)
+            print(s.most_common(10))
+            total_count = sum(s.values())
+            nb_per_duplicata = Counter(s.values())
+            for duplicata, count in sorted(nb_per_duplicata.items(), key=lambda d: d[0]):
+                print('\t'+f"{duplicata} × {count}".ljust(14), f"{round(count/total_count*100,3)}% du total")
+            total_duplicata = sum(c for d, c in nb_per_duplicata.items() if d>1)
+            print(f"Collision : {round(total_duplicata/total_count*100,3)}%")
 
         print()
         show_elements_repartition(words.NOUNS,      leading='     nouns: ')
         show_elements_repartition(words.ADJECTIVES, leading='adjectives: ', header=False)
-        show_elements_repartition(stats,            leading='duplicates: ', header=False, keep_count_on=lambda c: c>1, elem_to_elems=na2na)
+        for exp, expstats in stats.items():
+            show_elements_repartition(expstats,     leading= f'EXP{exp}: '.ljust(len('adjectives: ')), header=False, keep_count_on=lambda c: c>0, elem_to_elems=na2na)
+
+        print(
+            "Cette expérience démontre que la méthode sum introduit un biais important dans les données.\n"
+            "La somme sur les bytes va effectivement créer une gaussienne dans la répartition des entiers générés, et donc un biais de sur-représentation de certains indexes.\n"
+            "La conséquence pratique est l'augmentation du nombre de collisions."
+        )
 
--- lully/hashing.py
+++ lully/hashing.py
@@ -1,19 +1,26 @@
 import uuid
 import hashlib
 import functools
+from warnings import warn
 from typing import Union
 from .words import NOUNS, ADJECTIVES
 
 
 def human_code(value: Union[str, uuid.UUID], *, CHUNK_SIZE: int = 40, method_sum: bool = False) -> str:
+    if method_sum:
+        warn(f"Sum method is not statistically sound. You will see a sur-representation of some indexes, thus a increase in collision number")
     if isinstance(value, uuid.UUID):
         value: str = str(value)
     h = hashlib.blake2b(digest_size=CHUNK_SIZE, usedforsecurity=False)
     h.update(str(value).encode())
-    for val in h.digest():
-        assert val >= 0, val
-    vals = [int(val if val > 0 else 1) for val in h.digest()]
-    assert len(vals) == CHUNK_SIZE
-    noun_vals, adj_vals = sum(vals[:CHUNK_SIZE//2]), sum(vals[CHUNK_SIZE//2:])
+    if method_sum:
+        for val in h.digest():
+            assert val >= 0, val
+        vals = [int(val if val > 0 else 1) for val in h.digest()]
+        assert len(vals) == CHUNK_SIZE
+        noun_vals, adj_vals = sum(vals[:CHUNK_SIZE//2]), sum(vals[CHUNK_SIZE//2:])
+    else:
+        noun_vals = int.from_bytes(h.digest()[:CHUNK_SIZE//2], byteorder='big')
+        adj_vals  = int.from_bytes(h.digest()[CHUNK_SIZE//2:], byteorder='big')
     return f'{NOUNS[noun_vals % len(NOUNS)]} {ADJECTIVES[adj_vals % len(ADJECTIVES)]}'
 
