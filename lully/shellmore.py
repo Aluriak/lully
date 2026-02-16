df5d13fa154626dee9b3f85a94464ce533ec6408
Lucas Bourneuf <lucas.bourneuf@chu-brest.fr>
2026-02-16 15:29:30 +0100

    feat(user input yes)

--- lully/shellmore.py
+++ lully/shellmore.py
@@ -16,3 +16,18 @@ def envvar_is_true(envvar: str, *, default: bool = False) -> bool:
 
 def envvar_is_false(envvar: str, *, default: bool = False) -> bool:
     return not envvar_is_true(envvar, default=default)
+
+
+def user_inputs_yes(text: str, default: str = 'yes', true_if: str = 'yes', suffix: str = '[{y}/{n}] ') -> bool:
+    YESY_VALUES = 'y', 'yes', 'yeah', 'o', 'oui', '1', True
+    NOSY_VALUES = 'n', 'no', 'non', 'nop', 'nope', '0', False
+    answer = input(text + suffix.format(
+        y='Y' if default in YESY_VALUES else 'y',
+        n='N' if default in NOSY_VALUES else 'n',
+    )).strip().lower()
+    if not answer or answer not in YESY_VALUES+NOSY_VALUES:
+        answer = default
+    if true_if in YESY_VALUES:
+        return answer in YESY_VALUES
+    else:
+        return answer in NOSY_VALUES
